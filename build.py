28b8801776fcbb2bb97f956ca99b86ff0d1cf8d7
linkliang <linckyliang@gmail.com>
2019-05-23 12:46:43 -0700

    build script

---
+++ build.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+branch = sys.argv[1]
+
+git_pull_command = 'git pull branch'
+os.system(git_pull_command)
+
+with open('package.json', 'r') as file:
+    data = file.read().replace('{{branch_name}}', branch)
+with open('package.json', 'w') as file:
+    file.write(data)
+    file.close()
+
+build_command = 'npm run start'
+os.system(build_command)
